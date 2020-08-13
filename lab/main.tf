861f5917b793eccb45db38e6ea4e1dc95396a1f9
rayanah <ror.s.m2010@hotmail.com>
2020-08-13 13:17:35 +0300

    change to remote-exec fix

--- lab/main.tf
+++ lab/main.tf
@@ -177,7 +177,7 @@ resource "aws_instance" "webserver" {
   depends_on                  = [aws_instance.api]
   
    provisioner "remote-exec" {
-    command = "echo \"${aws_instance.api.public_ip}\" > index.html"
+     inline  = "echo \"${aws_instance.api.public_ip}\" > index.html"
   }
 }
 
